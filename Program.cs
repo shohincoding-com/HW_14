9e43febabead8fcfd8040627edcd6c7404b93faf
shohincoding-com <63291350+shohincoding-com@users.noreply.github.com>
2020-04-21 09:01:40 +0500

    Написал условия для мат.функ. Проект готов!!!

--- Program.cs
+++ Program.cs
@@ -11,8 +11,42 @@ namespace HW_14
             Console.WriteLine("Введите val_1 = ");
             double val_1 = double.Parse(Console.ReadLine());
 
+            Console.WriteLine("Вибырите математическую функцию:  ");
+            var MathFunc = Console.ReadLine();
+
             Console.WriteLine("Введите val_2 = ");
             double val_2 = double.Parse(Console.ReadLine());
+
+            if (MathFunc == "-")
+            {
+                DelegateCalc<double> minus = CalcHelper.MINUS;
+                Console.WriteLine($"{val_1} - {val_2} = {minus.Invoke(val_1,val_2)}");
+            }
+
+            if (MathFunc == "+")
+            {
+                DelegateCalc<double> plus = CalcHelper.PLUS;
+                Console.WriteLine($"{val_1} + {val_2} = {plus.Invoke(val_1,val_2)}");
+            }
+
+            if (MathFunc == "/")
+            {
+                DelegateCalc<double> divis = CalcHelper.DIVIS;
+                Console.WriteLine($"{val_1} / {val_2} = {divis.Invoke(val_1,val_2)}");
+            }
+
+            if (MathFunc == "*")
+            {
+                DelegateCalc<double> milt = CalcHelper.MILT;
+                Console.WriteLine($"{val_1} * {val_2} = {milt.Invoke(val_1,val_2)}");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+
+
+
         }
     }
 }
