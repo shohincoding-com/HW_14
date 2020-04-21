2bf0bf8e28964bd3db6ea14294673a1a6993569c
shohincoding-com <63291350+shohincoding-com@users.noreply.github.com>
2020-04-21 08:43:07 +0500

    Обявил delegate и вводил значение для val_1, val2

--- Program.cs
+++ Program.cs
@@ -5,9 +5,14 @@ namespace HW_14
 {
     class Program
     {
+        delegate S DelegateCalc <S> (S val_1, S val_2);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите val_1 = ");
+            double val_1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите val_2 = ");
+            double val_2 = double.Parse(Console.ReadLine());
         }
     }
 }
